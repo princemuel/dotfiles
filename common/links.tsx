0e5a14d4a93beb433f50de023fcbc9743858f764
princemuel <vansomecsam@gmail.com>
2022-11-03 03:06:03 +0100

    Update links and icon objects

--- common/links.tsx
+++ common/links.tsx
@@ -1,4 +1,12 @@
-import { IconGithubSVG, IconLinkedInSVG, IconTwitterSVG } from './images';
+import * as React from 'react';
+import {
+  IconGithubSVG,
+  IconLinkedInSVG,
+  IconTwitterSVG,
+  LogoSVG,
+} from './images';
+
+export interface IconProps extends React.ComponentPropsWithoutRef<'svg'> {}
 
 export const links = {
   navigation: [
@@ -18,24 +26,26 @@ export const links = {
       url: '/speakers',
     },
   ],
-
+  logo: {
+    icon: (props: IconProps) => <LogoSVG {...props} />,
+  },
   social: [
     {
       id: 'icon-github',
       url: 'https://github.com/princemuel',
-      icon: IconGithubSVG,
+      icon: (props: IconProps) => <IconGithubSVG {...props} />,
       alt: 'github',
     },
     {
       id: 'icon-twitter',
       url: 'https://www.twitter.com/iamprincemuel',
-      icon: IconTwitterSVG,
+      icon: (props: IconProps) => <IconTwitterSVG {...props} />,
       alt: 'twitter',
     },
     {
       id: 'icon-linkedin',
       url: 'https://www.linkedin.com/in/princemuel',
-      icon: IconLinkedInSVG,
+      icon: (props: IconProps) => <IconLinkedInSVG {...props} />,
       alt: 'linkedin',
     },
   ],
