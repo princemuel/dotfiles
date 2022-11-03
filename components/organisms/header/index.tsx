26860877b6c91837053e186fe25913e97cdf07d7
princemuel <vansomecsam@gmail.com>
2022-11-03 03:43:18 +0100

    Remove images for svg

---
+++ components/organisms/header/index.tsx
@@ -0,0 +1,26 @@
+import { links } from 'common';
+import { Logo, NavLink } from 'components/atoms';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <Logo className='' />
+
+      <nav>
+        <ul role={'list'} className='' aria-label='Primary Navigation'>
+          {links?.navigation?.map((link) => (
+            <li key={link.text}>
+              <NavLink activeClassName='' href={link.url}>
+                <a>{link.text}</a>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export { Header };
