8b82b017622d478b63e8f3ff7736b1cf256d0092
princemuel <vansomecsam@gmail.com>
2022-11-16 20:03:57 +0100

    Finished styling footer

--- components/organisms/footer/index.tsx
+++ components/organisms/footer/index.tsx
@@ -5,7 +5,7 @@ import { SocialLinks } from 'components/molecules';
 const Footer = () => {
   return (
     <footer className='h-container mt-40 bg-primary-300 w-full-shadow'>
-      <section className="relative py-16 text-neutral-100/75 before:content-[''] before:absolute before:top-0 before:left-2/4 before:w-40 before:h-2 before:bg-primary-100 before:-translate-x-2/4 before:md:left-0 before:md:translate-x-0">
+      <section className="relative py-32 text-neutral-100/75 before:content-[''] before:absolute before:top-0 before:left-2/4 before:w-40 before:h-2 before:bg-primary-100 before:-translate-x-2/4 before:md:left-0 before:md:translate-x-0">
         <div className='flex flex-col lg:flex-row items-center md:items-start lg:justify-between gap-12 mb-12 text-center md:text-left'>
           <Logo className='text-neutral-100' />
           {/* NAVIGATION */}
@@ -29,8 +29,8 @@ const Footer = () => {
           </nav>
         </div>
 
-        <div className='grid justify-items-center md:justify-items-start gap-12 mt-12 body-200 text-center'>
-          <p>
+        <div className='grid md:grid-areas-ipad lg:grid-areas-desktop md:grid-cols-2 justify-items-center md:justify-items-start gap-12 mt-12 body-200 text-center md:text-left'>
+          <p className='md:grid-in-info max-w-[60ch]'>
             Hi there! Thanks for sticking with me till this point. If
             you&quot;re looking for a fast, perfomant, user-friendly and
             accessible website to represent your product or business, a
@@ -38,7 +38,7 @@ const Footer = () => {
             out. I will do my best to respond. 😊 The quickest way to reach me
             is via email.
           </p>
-          <p>
+          <p className='md:grid-in-copy'>
             Copyright &copy; {new Date().getFullYear()} Prince Muel. All Rights
             Reserved
           </p>
