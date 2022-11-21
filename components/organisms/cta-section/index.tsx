e9b9e88d435f08e3d0ade4ebcb4f29b5c0fc91ec
princemuel <vansomecsam@gmail.com>
2022-11-21 16:56:39 +0100

    Added CTA section

---
+++ components/organisms/cta-section/index.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const CTASection = (props: Props) => {
+  return (
+    <section>
+      <h2>Interested in doing a project together?</h2>
+    </section>
+  );
+};
+
+export { CTASection };
--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,1 +1,2 @@
-export * from './media-object';
+export * from './cta-section';
+export * from './projects';
