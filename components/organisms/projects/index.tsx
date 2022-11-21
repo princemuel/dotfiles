2cd6d2c4ae39c7ef25801680043cfb7c39e30b54
princemuel <vansomecsam@gmail.com>
2022-11-21 16:51:41 +0100

    Add project card list

---
+++ components/organisms/projects/index.tsx
@@ -0,0 +1,20 @@
+import { ProjectCard } from 'components/molecules';
+
+type Props = {};
+
+const projects = Array(4).fill(1);
+
+const Projects = (props: Props) => {
+  return (
+    <section
+      className='h-container mt-40 space-y-40 > * + *'
+      aria-label='projects list'
+    >
+      {projects?.map((project, i) => {
+        return <ProjectCard key={project + i} isPriority={i === 0} />;
+      })}
+    </section>
+  );
+};
+
+export { Projects };
