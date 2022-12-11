8321662216602a70185185418555efb05d458ffa
Prince Muel <vansomecsam@gmail.com>
2022-12-11 02:59:15 +0100

    feat: add project.d.ts to types directory

--- types/project.d.ts
+++ types/project.d.ts
@@ -7,6 +7,7 @@ declare type $ElementProps<T> = T extends React.ComponentType<infer Props>
 // Usage
 // import { CSSProperties } from 'react';
 // const Box = (props: CSSProperties) => <div style={props} />;
+
 // const Card = (
 //   { title, children, ...props }: { title: string } & $ElementProps<typeof Box> // new utility, see below
 // ) => (
